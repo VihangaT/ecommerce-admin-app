30dd2d90202f55c163444d5b64790b7dbb0ff994
vihanga <liyanage.vihanga99@gmail.com>
2023-12-11 23:26:46 +0530

    implemented the API to create Products

---
+++ pages/products/new.js
@@ -0,0 +1,44 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useState } from "react";
+
+export default function NewProduct() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  async function createProduct(ev) {
+    ev.preventDefault();
+    const data = { title, description, price };
+    await axios.post("/api/products", data);
+  }
+  return (
+    <Layout>
+      <form onSubmit={createProduct}>
+        <h1>New Product</h1>
+        <label>Product Name</label>
+        <input
+          type="text"
+          placeholder="Product Name"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label>Description</label>
+        <textarea
+          placeholder="Description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        ></textarea>
+        <label>Price in LKR</label>
+        <input
+          type="text"
+          placeholder="Price"
+          value={price}
+          onChange={(event) => setPrice(event.target.value)}
+        />
+        <button type="submit" className="btn-primary">
+          Save
+        </button>
+      </form>
+    </Layout>
+  );
+}
