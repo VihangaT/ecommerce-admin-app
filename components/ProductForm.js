8b4d42e92cdf720f975f9d830cda941312bd1402
vihanga <liyanage.vihanga99@gmail.com>
2023-12-30 00:46:23 +0530

    implemented update product

---
+++ components/ProductForm.js
@@ -0,0 +1,57 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function ProductForm({
+  _id,
+  title: existingTitle,
+  description: existingDescription,
+  price: existingPrice,
+}) {
+  const [title, setTitle] = useState(existingTitle || "");
+  const [description, setDescription] = useState(existingDescription || "");
+  const [price, setPrice] = useState(existingPrice || "");
+  const [goToProducts, setGoToProducts] = useState(false);
+  const router = useRouter();
+  async function saveProduct(ev) {
+    const data = { title, description, price };
+    ev.preventDefault();
+    if (_id) {
+      await axios.put("/api/products", { ...data, _id });
+    } else {
+      await axios.post("/api/products", data);
+    }
+    setGoToProducts(true);
+  }
+  if (goToProducts) {
+    router.push("/products");
+  }
+  return (
+    <form onSubmit={saveProduct}>
+      <label>Product Name</label>
+      <input
+        type="text"
+        placeholder="Product Name"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <label>Description</label>
+      <textarea
+        placeholder="Description"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      ></textarea>
+      <label>Price in LKR</label>
+      <input
+        type="text"
+        placeholder="Price"
+        value={price}
+        onChange={(event) => setPrice(event.target.value)}
+      />
+      <button type="submit" className="btn-primary">
+        Save
+      </button>
+    </form>
+  );
+}
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,51 +1,11 @@
 import Layout from "@/components/Layout";
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import ProductForm from "@/components/ProductForm";
 
 export default function NewProduct() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [goToProducts, setGoToProducts] = useState(false);
-  const router = useRouter();
-  async function createProduct(ev) {
-    ev.preventDefault();
-    const data = { title, description, price };
-    await axios.post("/api/products", data);
-    setGoToProducts(true);
-  }
-  if (goToProducts) {
-    router.push("/products");
-  }
   return (
     <Layout>
-      <form onSubmit={createProduct}>
-        <h1>New Product</h1>
-        <label>Product Name</label>
-        <input
-          type="text"
-          placeholder="Product Name"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-        />
-        <label>Description</label>
-        <textarea
-          placeholder="Description"
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-        ></textarea>
-        <label>Price in LKR</label>
-        <input
-          type="text"
-          placeholder="Price"
-          value={price}
-          onChange={(event) => setPrice(event.target.value)}
-        />
-        <button type="submit" className="btn-primary">
-          Save
-        </button>
-      </form>
+      <h1>New Product</h1>
+      <ProductForm />
     </Layout>
   );
 }
