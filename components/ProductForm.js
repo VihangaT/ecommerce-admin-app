2ffa87e6287f552d1ef9e44dc1f270736cb505b4
vihanga <liyanage.vihanga99@gmail.com>
2024-01-01 23:39:48 +0530

    Started image uploading from admin

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -8,6 +8,7 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -24,6 +25,15 @@ export default function ProductForm({
     }
     setGoToProducts(true);
   }
+  function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      files.forEach((file) => {
+        data.append("file", file);
+      });
+    }
+  }
   if (goToProducts) {
     router.push("/products");
   }
@@ -36,6 +46,28 @@ export default function ProductForm({
         value={title}
         onChange={(event) => setTitle(event.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 cursor-pointer flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" className="hidden" onChange={uploadImages}></input>
+        </label>
+        {!images?.length && <div>No photos in this product</div>}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="Description"
