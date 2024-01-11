51bd9d465ad906803c9b6e3fac3b6e3d49e9197a
vihanga <liyanage.vihanga99@gmail.com>
2024-01-11 23:10:54 +0530

    initiated the integration with AWS s3 bucket

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,8 @@
 import multiparty from "multiparty";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import fs from "fs";
+const bucketName = "next-ecommerce-clothing";
+import mime from "mime-types";
 
 export default async function handle(req, res) {
   const form = new multiparty.Form();
@@ -9,7 +13,34 @@ export default async function handle(req, res) {
     });
   });
 
-  return res.json("ok");
+  const client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const links = [];
+
+  for (const file of files.file) {
+    const extension = file.originalFilename.split(".").pop();
+    const newFileName = Date.now() + "." + extension;
+    console.log({ extension, file });
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFileName,
+        Body: fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: mime.lookup(file.path),
+      })
+    );
+    const link = `https://${bucketName}.s3.amazonaws.com/${newFileName}`;
+    links.push(link);
+  }
+
+  return res.json({ links });
 }
 
 export const config = {
