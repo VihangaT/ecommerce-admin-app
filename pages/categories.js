44e78faf4a5a68acd4f056104bbf872ceb65116e
vihanga <liyanage.vihanga99@gmail.com>
2024-01-20 13:49:34 +0530

    Implemented to add and update categories

--- pages/categories.js
+++ pages/categories.js
@@ -3,49 +3,101 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Categories() {
-  const [name, setName] = useState("");
-  const [categories, setCategories] = useState([]);
-  useEffect(() => {
-    axios.get("/api/categories").then((result) => {
-      setCategories(result.data);
-    });
-  }, []);
-  async function saveCategory(ev) {
-    ev.preventDefault();
-    await axios.post("/api/categories", { name });
-    setName("");
-  }
-  return (
-    <Layout>
-      <label>New Category Name</label>
-      <h1>Categories</h1>
-      <form onSubmit={saveCategory} className="flex gap-1">
-        <input
-          className="mb-0"
-          type="text"
-          placeholder={"Category Name"}
-          onChange={(ev) => setName(ev.target.value)}
-          value={name}
-        />
-        <button className="btn-primary py-1" type="submit">
-          Save
-        </button>
-      </form>
-      <table className="basic mt-4">
-        <thead>
-          <tr>
-            <td>Category Name</td>
-          </tr>
-        </thead>
-        <tbody>
-          {categories.length > 0 &&
-            categories.map((category) => (
-              <tr key={category._id}>
-                <td>{category.name}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    </Layout>
-  );
+    const [editedCategory, setEditedCategory] = useState(null);
+    const [name, setName] = useState("");
+    const [parentCategory, setParentCategory] = useState("");
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+      fetchCategories();
+    }, []);
+    async function saveCategory(ev) {
+      ev.preventDefault();
+      const data = { name, parentCategory };
+
+      if (editedCategory) {
+        data._id = editedCategory._id;
+        await axios.put("/api/categories", data);
+        setEditedCategory(null);
+      } else {
+        await axios.post("/api/categories", data);
+      }
+      setName("");
+      setParentCategory("");
+      fetchCategories();
+    }
+
+    function fetchCategories() {
+      axios.get("/api/categories").then((result) => {
+        setCategories(result.data);
+      });
+    }
+
+    function editCategory(category) {
+      setEditedCategory(category);
+      setName(category.name);
+      setParentCategory(category?.parent?._id);
+    }
+
+    return (
+      <Layout>
+        <label>
+          {editedCategory
+            ? `Edit Category ${editedCategory?.name}`
+            : "Create New Category"}
+        </label>
+        <h1>Categories</h1>
+        <form onSubmit={saveCategory} className="flex gap-1">
+          <input
+            className="mb-0"
+            type="text"
+            placeholder={"Category Name"}
+            onChange={(ev) => setName(ev.target.value)}
+            value={name}
+          />
+          <select
+            className="mb-0"
+            onChange={(ev) => setParentCategory(ev.target.value)}
+            value={parentCategory}
+          >
+            <option value="">No Parent Category</option>
+            {categories.length > 0 &&
+              categories.map((category) => (
+                <option key={category._id} value={category._id}>
+                  {category.name}
+                </option>
+              ))}
+          </select>
+          <button className="btn-primary py-1" type="submit">
+            Save
+          </button>
+        </form>
+        <table className="basic mt-4">
+          <thead>
+            <tr>
+              <td>Category Name</td>
+              <td>Parent Category</td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+            {categories.length > 0 &&
+              categories.map((category) => (
+                <tr key={category._id}>
+                  <td>{category.name}</td>
+                  <td>{category?.parent?.name}</td>
+                  <td>
+                    <button
+                      onClick={() => editCategory(category)}
+                      className="btn-primary mr-1"
+                    >
+                      Edit
+                    </button>
+                    <button className="btn-primary">Delete</button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </Layout>
+    );
 }
